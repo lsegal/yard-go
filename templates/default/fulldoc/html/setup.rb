882446f279a63970073f73334d7b63da2cc81f20
Loren Segal <lsegal@soen.ca>
2015-07-01 11:34:15 -0700

    Add Package/Method/Function listings

--- templates/default/fulldoc/html/setup.rb
+++ templates/default/fulldoc/html/setup.rb
@@ -1,3 +1,26 @@
 def run_verifier(objs)
   super(objs.reject {|t| t.type == :bare_struct })
 end
+
+def generate_method_list
+  @items = prune_method_listing(Registry.all(:method).select {|m| m.scope == :instance }, false)
+  @items = @items.sort_by {|m| m.name.to_s }
+  @list_title = "Method List"
+  @list_type = "method"
+  generate_list_contents
+end
+
+def generate_function_list
+  @items = prune_method_listing(Registry.all(:method).select {|m| m.scope == :instance }, false)
+  @items = @items.sort_by {|m| m.name.to_s }
+  @list_title = "Function List"
+  @list_type = "function"
+  generate_list_contents
+end
+
+def generate_package_list
+  @items = options.objects if options.objects
+  @list_title = "Package List"
+  @list_type = "package"
+  generate_list_contents
+end
--- templates/default/layout/html/setup.rb
+++ templates/default/layout/html/setup.rb
@@ -2,6 +2,13 @@ def stylesheets
   super + %w(css/highlight.github.css)
 end
 
+def menu_lists
+[ { :type => 'package', :title => 'Packages', :search_title => 'Package List' },
+  { :type => 'function', :title => 'Functions', :search_title => 'Function List' },
+  { :type => 'method', :title => 'Methods', :search_title => 'Method List' },
+  { :type => 'file', :title => 'Files', :search_title => 'File List' } ]
+end
+
 def index
   @objects_by_letter = {}
   objects = Registry.all(:struct).sort_by {|o| o.name.to_s }
