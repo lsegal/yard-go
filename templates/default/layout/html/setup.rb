cfe0759b48a133c8bc7164bf0b9a2b584e56e730
Loren Segal <lsegal@soen.ca>
2015-07-01 11:39:16 -0700

    Correctly reset left frame nav_url

--- templates/default/layout/html/setup.rb
+++ templates/default/layout/html/setup.rb
@@ -16,3 +16,19 @@ def index
   objects.each {|o| (@objects_by_letter[o.name.to_s[0,1].upcase] ||= []) << o }
   erb(:index)
 end
+
+def layout
+  @nav_url = url_for_list(!@file || options.index ? 'package' : 'file')
+
+  if !object || object.is_a?(String)
+    @path = nil
+  elsif @file
+    @path = @file.path
+  elsif !object.is_a?(YARD::CodeObjects::NamespaceObject)
+    @path = object.parent.path
+  else
+    @path = object.path
+  end
+
+  erb(:layout)
+end
